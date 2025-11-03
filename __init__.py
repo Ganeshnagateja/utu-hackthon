d12f81c130cf909d98322099563545da7ff9c617
Ganeshnagateja <ganeshboddu52@gmail.com>
2025-11-03 23:29:41 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,155 +1,53 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Google LLC
+# Copyright 2016 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-from google.ai.generativelanguage_v1beta3 import gapic_version as package_version
 
-__version__ = package_version.__version__
+"""Google Auth Library for Python."""
 
+import logging
+import sys
+import warnings
 
-from .services.discuss_service import DiscussServiceAsyncClient, DiscussServiceClient
-from .services.model_service import ModelServiceAsyncClient, ModelServiceClient
-from .services.permission_service import (
-    PermissionServiceAsyncClient,
-    PermissionServiceClient,
-)
-from .services.text_service import TextServiceAsyncClient, TextServiceClient
-from .types.citation import CitationMetadata, CitationSource
-from .types.discuss_service import (
-    CountMessageTokensRequest,
-    CountMessageTokensResponse,
-    Example,
-    GenerateMessageRequest,
-    GenerateMessageResponse,
-    Message,
-    MessagePrompt,
-)
-from .types.model import Model
-from .types.model_service import (
-    CreateTunedModelMetadata,
-    CreateTunedModelRequest,
-    DeleteTunedModelRequest,
-    GetModelRequest,
-    GetTunedModelRequest,
-    ListModelsRequest,
-    ListModelsResponse,
-    ListTunedModelsRequest,
-    ListTunedModelsResponse,
-    UpdateTunedModelRequest,
-)
-from .types.permission import Permission
-from .types.permission_service import (
-    CreatePermissionRequest,
-    DeletePermissionRequest,
-    GetPermissionRequest,
-    ListPermissionsRequest,
-    ListPermissionsResponse,
-    TransferOwnershipRequest,
-    TransferOwnershipResponse,
-    UpdatePermissionRequest,
-)
-from .types.safety import (
-    ContentFilter,
-    HarmCategory,
-    SafetyFeedback,
-    SafetyRating,
-    SafetySetting,
-)
-from .types.text_service import (
-    BatchEmbedTextRequest,
-    BatchEmbedTextResponse,
-    CountTextTokensRequest,
-    CountTextTokensResponse,
-    Embedding,
-    EmbedTextRequest,
-    EmbedTextResponse,
-    GenerateTextRequest,
-    GenerateTextResponse,
-    TextCompletion,
-    TextPrompt,
-)
-from .types.tuned_model import (
-    Dataset,
-    Hyperparameters,
-    TunedModel,
-    TunedModelSource,
-    TuningExample,
-    TuningExamples,
-    TuningSnapshot,
-    TuningTask,
+from google.auth import version as google_auth_version
+from google.auth._default import (
+    default,
+    load_credentials_from_dict,
+    load_credentials_from_file,
 )
 
-__all__ = (
-    "DiscussServiceAsyncClient",
-    "ModelServiceAsyncClient",
-    "PermissionServiceAsyncClient",
-    "TextServiceAsyncClient",
-    "BatchEmbedTextRequest",
-    "BatchEmbedTextResponse",
-    "CitationMetadata",
-    "CitationSource",
-    "ContentFilter",
-    "CountMessageTokensRequest",
-    "CountMessageTokensResponse",
-    "CountTextTokensRequest",
-    "CountTextTokensResponse",
-    "CreatePermissionRequest",
-    "CreateTunedModelMetadata",
-    "CreateTunedModelRequest",
-    "Dataset",
-    "DeletePermissionRequest",
-    "DeleteTunedModelRequest",
-    "DiscussServiceClient",
-    "EmbedTextRequest",
-    "EmbedTextResponse",
-    "Embedding",
-    "Example",
-    "GenerateMessageRequest",
-    "GenerateMessageResponse",
-    "GenerateTextRequest",
-    "GenerateTextResponse",
-    "GetModelRequest",
-    "GetPermissionRequest",
-    "GetTunedModelRequest",
-    "HarmCategory",
-    "Hyperparameters",
-    "ListModelsRequest",
-    "ListModelsResponse",
-    "ListPermissionsRequest",
-    "ListPermissionsResponse",
-    "ListTunedModelsRequest",
-    "ListTunedModelsResponse",
-    "Message",
-    "MessagePrompt",
-    "Model",
-    "ModelServiceClient",
-    "Permission",
-    "PermissionServiceClient",
-    "SafetyFeedback",
-    "SafetyRating",
-    "SafetySetting",
-    "TextCompletion",
-    "TextPrompt",
-    "TextServiceClient",
-    "TransferOwnershipRequest",
-    "TransferOwnershipResponse",
-    "TunedModel",
-    "TunedModelSource",
-    "TuningExample",
-    "TuningExamples",
-    "TuningSnapshot",
-    "TuningTask",
-    "UpdatePermissionRequest",
-    "UpdateTunedModelRequest",
-)
+
+__version__ = google_auth_version.__version__
+
+
+__all__ = ["default", "load_credentials_from_file", "load_credentials_from_dict"]
+
+
+class Python37DeprecationWarning(DeprecationWarning):  # pragma: NO COVER
+    """
+    Deprecation warning raised when Python 3.7 runtime is detected.
+    Python 3.7 support will be dropped after January 1, 2024.
+    """
+
+    pass
+
+
+# Checks if the current runtime is Python 3.7.
+if sys.version_info.major == 3 and sys.version_info.minor == 7:  # pragma: NO COVER
+    message = (
+        "After January 1, 2024, new releases of this library will drop support "
+        "for Python 3.7."
+    )
+    warnings.warn(message, Python37DeprecationWarning)
+
+# Set default logging handler to avoid "No handler found" warnings.
+logging.getLogger(__name__).addHandler(logging.NullHandler())
