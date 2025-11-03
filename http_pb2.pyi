28cd3cb2c2c8993721089cf55f6314db31bc275a
Ganeshnagateja <ganeshboddu52@gmail.com>
2025-11-03 23:39:48 +0530

    Add files via upload

--- http_pb2.pyi
+++ http_pb2.pyi
@@ -24,71 +24,48 @@ from google.protobuf.internal import containers as _containers
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class Http(_message.Message):
-    __slots__ = ("rules", "fully_decode_reserved_expansion")
-    RULES_FIELD_NUMBER: _ClassVar[int]
-    FULLY_DECODE_RESERVED_EXPANSION_FIELD_NUMBER: _ClassVar[int]
-    rules: _containers.RepeatedCompositeFieldContainer[HttpRule]
-    fully_decode_reserved_expansion: bool
-    def __init__(
-        self,
-        rules: _Optional[_Iterable[_Union[HttpRule, _Mapping]]] = ...,
-        fully_decode_reserved_expansion: bool = ...,
-    ) -> None: ...
-
-class HttpRule(_message.Message):
-    __slots__ = (
-        "selector",
-        "get",
-        "put",
-        "post",
-        "delete",
-        "patch",
-        "custom",
-        "body",
-        "response_body",
-        "additional_bindings",
-    )
-    SELECTOR_FIELD_NUMBER: _ClassVar[int]
-    GET_FIELD_NUMBER: _ClassVar[int]
-    PUT_FIELD_NUMBER: _ClassVar[int]
-    POST_FIELD_NUMBER: _ClassVar[int]
-    DELETE_FIELD_NUMBER: _ClassVar[int]
-    PATCH_FIELD_NUMBER: _ClassVar[int]
-    CUSTOM_FIELD_NUMBER: _ClassVar[int]
+class HttpRequest(_message.Message):
+    __slots__ = ("method", "uri", "headers", "body")
+    METHOD_FIELD_NUMBER: _ClassVar[int]
+    URI_FIELD_NUMBER: _ClassVar[int]
+    HEADERS_FIELD_NUMBER: _ClassVar[int]
     BODY_FIELD_NUMBER: _ClassVar[int]
-    RESPONSE_BODY_FIELD_NUMBER: _ClassVar[int]
-    ADDITIONAL_BINDINGS_FIELD_NUMBER: _ClassVar[int]
-    selector: str
-    get: str
-    put: str
-    post: str
-    delete: str
-    patch: str
-    custom: CustomHttpPattern
-    body: str
-    response_body: str
-    additional_bindings: _containers.RepeatedCompositeFieldContainer[HttpRule]
+    method: str
+    uri: str
+    headers: _containers.RepeatedCompositeFieldContainer[HttpHeader]
+    body: bytes
     def __init__(
         self,
-        selector: _Optional[str] = ...,
-        get: _Optional[str] = ...,
-        put: _Optional[str] = ...,
-        post: _Optional[str] = ...,
-        delete: _Optional[str] = ...,
-        patch: _Optional[str] = ...,
-        custom: _Optional[_Union[CustomHttpPattern, _Mapping]] = ...,
-        body: _Optional[str] = ...,
-        response_body: _Optional[str] = ...,
-        additional_bindings: _Optional[_Iterable[_Union[HttpRule, _Mapping]]] = ...,
+        method: _Optional[str] = ...,
+        uri: _Optional[str] = ...,
+        headers: _Optional[_Iterable[_Union[HttpHeader, _Mapping]]] = ...,
+        body: _Optional[bytes] = ...,
     ) -> None: ...
 
-class CustomHttpPattern(_message.Message):
-    __slots__ = ("kind", "path")
-    KIND_FIELD_NUMBER: _ClassVar[int]
-    PATH_FIELD_NUMBER: _ClassVar[int]
-    kind: str
-    path: str
+class HttpResponse(_message.Message):
+    __slots__ = ("status", "reason", "headers", "body")
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    REASON_FIELD_NUMBER: _ClassVar[int]
+    HEADERS_FIELD_NUMBER: _ClassVar[int]
+    BODY_FIELD_NUMBER: _ClassVar[int]
+    status: int
+    reason: str
+    headers: _containers.RepeatedCompositeFieldContainer[HttpHeader]
+    body: bytes
     def __init__(
-        self, kind: _Optional[str] = ..., path: _Optional[str] = ...
+        self,
+        status: _Optional[int] = ...,
+        reason: _Optional[str] = ...,
+        headers: _Optional[_Iterable[_Union[HttpHeader, _Mapping]]] = ...,
+        body: _Optional[bytes] = ...,
+    ) -> None: ...
+
+class HttpHeader(_message.Message):
+    __slots__ = ("key", "value")
+    KEY_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    key: str
+    value: str
+    def __init__(
+        self, key: _Optional[str] = ..., value: _Optional[str] = ...
     ) -> None: ...
